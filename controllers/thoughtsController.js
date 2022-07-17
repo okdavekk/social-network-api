6f40f3bd9117ac8043437782326a52f47131895d
okdavekk <okdavekk@gmail.com>
2022-07-17 15:49:16 -0400

    Updated usersController.js

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -1,9 +1,49 @@
-const Thought = require('../models/Thought');
+const Thought = require("../models/Thought");
 
-// getAllThoughts,
-// getASingleThoughtByID,
-// createAThought,
-// updateAThoughtByID,
-// deleteAThoughtByID,
-// addAReactionByID,
-// removeAReactionByID,
+
+const thoughtController = {
+  getAllThoughts(req, res) {
+    Thought.find()
+        .sort({ createdAt: -1 })
+        .then((thoughtData) => {
+            res.json(thoughtData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+  },
+  getASingleThoughtByID(req, res) {
+    Thought.findOne({ _id: req.params.thoughtID })
+    .then((thoughtData) => {
+        res.json(thoughtData);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+  },
+
+  createAThought(req, res) {
+
+  },
+
+  updateAThoughtByID(req, res) {
+
+  },
+
+  deleteAThoughtByID(req, res) {
+
+  },
+
+  addAReactionByID(req, res) {
+
+  },
+
+  removeAReactionByID(req, res) {
+
+  },
+
+};
+
+module.exports = thoughtController;
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,9 +1,93 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
-// getAllUsers
-// getASingleUserByID
-// createAUser
-// updateAUserByID
-// deleteAUserByID
-// addAFriend
-// removeAFriendByID
+const userController = {
+  getAllUsers(req, res) {
+    User.find()
+      .select("-_v")
+      .then((userData) => {
+        res.json(userData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  getASingleUserByID(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-_v")
+      .populate("friends")
+      .populate("thoughts")
+      .then((userData) => {
+        res.json(userData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  createAUser(req, res) {
+    User.create(req.body)
+      .then((userData) => {
+        res.json(userData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  updateAUserByID(req, res) {
+    User.findByIdAndUpdate(
+      { _id: req.params.userID },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((userData) => {
+        res.json(userData);
+      })
+      .catch((err) => {
+        consoloe.log(err);
+        res.status(500).json(err);
+      });
+  },
+  deleteAUserByID(req, res) {
+    User.findOneAndDelete({ _id: req.params.userID })
+      .then(() => {
+        res.json({ message: "Deleted yo" });
+      })
+      .catch((err) => {
+        consoloe.log(err);
+        res.status(500).json(err);
+      });
+  },
+  addAFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendID } },
+      { new: true }
+    )
+      .then((userData) => {
+        res.json(userData);
+      })
+      .catch((err) => {
+        consoloe.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  removeAFriendByID(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendID } },
+      { new: true }
+    )
+      .then((userData) => {
+        res.json(userData);
+      })
+      .catch((err) => {
+        consoloe.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
+
+module.exports = userController;
