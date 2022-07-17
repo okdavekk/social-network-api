2dbd54e5d26d05f4c23538f43b93827c6c56fce6
okdavekk <okdavekk@gmail.com>
2022-07-17 14:19:04 -0400

    furtheres thought and user route.js files.

---
+++ routes/api/userRoute.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+
+const {
+  getAllUsers,
+  getASingleUserByID,
+  createAUser,
+  updateAUserByID,
+  deleteAUserByID,
+  addAFriend,
+  removeAFriendByID,
+} = require("../../controllers/usersController");
+
+router.route("/").get(getAllUsers).post(createAUser);
+
+router
+  .route("/:userID")
+  .get(getASingleUserByID)
+  .put(updateAUserByID)
+  .delete(deleteAUserByID);
+
+router.route("/:userID/friend").post(addAFriend);
+
+router.route("/:userId/friend/friendId").delete(removeAFriendByID);
+
+module.exports = router;
